f060ee06f7b0c5c95df719719ab98718f057f9b7
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-07 13:54:13 -0500

    saving changes to practice project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Link } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation/>
+    <Route exact path="/" components={Home}></Route>
+    <Route path="/About" components={About}></Route>
+    <Route path="/Contact" components={Navigation}></Route>
   </div>
 );
 
